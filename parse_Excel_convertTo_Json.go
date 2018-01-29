96408ad2e9464a20daf4e007c72a4d39b29d1c2b
Phu Nguyen <phu.nguyen@cfpb.gov>
2018-01-29 15:46:27 -0500

    code reformat using gofmt.

--- parse_Excel_convertTo_Json.go
+++ parse_Excel_convertTo_Json.go
@@ -1,61 +1,59 @@
 package main
 
 import (
-    "platformOps-EC/services"
-    "platformOps-EC/models"
-    "fmt"
-    "log"
-    "flag"
-    "os"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"platformOps-EC/models"
+	"platformOps-EC/services"
 )
-
 
 func main() {
 
-    var excelFileName, output string
+	var excelFileName, output string
 
-        flag.StringVar(&excelFileName, "i", "", "Input Excel baseline file. If missing, program will exit.")
-        flag.StringVar(&output, "o", "manifest.json", "Execution output location.")
-        flag.Parse()
+	flag.StringVar(&excelFileName, "i", "", "Input Excel baseline file. If missing, program will exit.")
+	flag.StringVar(&output, "o", "manifest.json", "Execution output location.")
+	flag.Parse()
 
-        if excelFileName == "" {
-                 fmt.Println("Missing input Excel file. Program will exit.")
-                  os.Exit(1)
-        }
+	if excelFileName == "" {
+		fmt.Println("Missing input Excel file. Program will exit.")
+		os.Exit(1)
+	}
 
-        if output == "manifest.json" {
-                     fmt.Println("Default to manifest.json")
+	if output == "manifest.json" {
+		fmt.Println("Default to manifest.json")
 
-        }
+	}
 
-    fmt.Println("Loading Excel file ", excelFileName)
+	fmt.Println("Loading Excel file ", excelFileName)
 
-    baseline, controls := services.LoadFromExcel(excelFileName)
-    var manifest [] models.EC_Manifest
+	baseline, controls := services.LoadFromExcel(excelFileName)
+	var manifest []models.EC_Manifest
 
-    fmt.Println("Converting to Json object")
+	fmt.Println("Converting to Json object")
 
+	for _, c := range controls {
 
-    for _, c := range controls {
+		m := models.EC_Manifest{Title: c.GetCategory(),
+			Baseline: baseline.GetName() + " at Req# " + c.GetRow_desc()}
+		manifest = append(manifest, m)
 
-            m := models.EC_Manifest{Title: c.GetCategory(),
-                                     Baseline: baseline.GetName() + " at Req# " + c.GetRow_desc()}
-            manifest = append(manifest, m)
+	}
 
-    }
+	//fmt.Println(models.ToJson(manifest))
 
-    //fmt.Println(models.ToJson(manifest))
+	file, err := os.Create(output)
+	if err != nil {
+		log.Fatal("Cannot create file", err)
+	}
+	defer file.Close()
 
-    file, err := os.Create(output)
-        if err != nil {
-            log.Fatal("Cannot create file", err)
-        }
-    defer file.Close()
+	fmt.Println("Writing Json Object to file")
 
-    fmt.Println("Writing Json Object to file")
+	fmt.Fprintf(file, "%v", models.ToJson(manifest))
 
-    fmt.Fprintf(file,"%v", models.ToJson(manifest))
-
-    fmt.Println("Done writing to output file at ", output)
+	fmt.Println("Done writing to output file at ", output)
 
 }
